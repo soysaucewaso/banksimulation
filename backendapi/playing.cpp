796ede6cb12977042866a75e2ed73fabf740211e
waso <waso@gitlab.cs470.local>
2024-07-01 16:21:55 -0700

    finally figured out threading on pistache would work if a thread waited for some time after sending a response

--- backendapi/playing.cpp
+++ backendapi/playing.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 #include <pistache/endpoint.h>
-#include <pthread.h>
+#include <thread>
 using namespace Pistache;
 
 class HelloHandler : public Http::Handler {
@@ -11,22 +11,29 @@ public:
     HTTP_PROTOTYPE(HelloHandler)
 
     void onRequest(const Http::Request& request, Http::ResponseWriter response) override{
+        
         std::cout << std::this_thread::get_id()<<std::endl;
         response.send(Http::Code::Ok, "Hello World\n");
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 };
 
 
 int main(){
     Address addr(Ipv4::any(), Port(9080));
-    auto opts = Http::Endpoint::options().threads(1);
-
-    auto server = std::make_shared<Http::Endpoint>(addr);
-    auto* serverPtr = &server;
-    server->init(opts);
-    server->setHandler(Http::make_handler<HelloHandler>());
+    auto opts = Http::Endpoint::options().threads(5);
+    Http::Endpoint server(addr);
+    server.init(opts);
+    server.setHandler(Http::make_handler<HelloHandler>());
     std::cout << std::this_thread::get_id()<<std::endl;
-    server->serve();
+    server.serveThreaded();
+    /*std::thread serverThread([server](){
+        std::cout << std::this_thread::get_id()<<std::endl;
+        server->serve();
+    });*/
+    std::this_thread::sleep_for(std::chrono::seconds(15));
+    
+    server.shutdown();
     return 0;
 }
 
